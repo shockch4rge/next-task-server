0c486d89ffbee9a1414938d7d2e32c6e994b37df
Favian <favouriteteo@gmail.com>
2023-04-05 11:54:15 +0800

    fix: add OTM relation on `Task`s

--- src/user/User.ts
+++ src/user/User.ts
@@ -1,8 +1,14 @@
-import { BaseEntity, Column, Entity, PrimaryColumn } from "typeorm";
+import { BaseEntity, Column, Entity, OneToMany, PrimaryColumn } from "typeorm";
+
+import { Task } from "../todo";
 
 @Entity()
 export class User extends BaseEntity {
     @PrimaryColumn() id!: string;
+
     @Column() name!: string;
+
     @Column() email!: string;
+    
+    @OneToMany(() => Task, task => task.author) tasks!: Task[];
 }
