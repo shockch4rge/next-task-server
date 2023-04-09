699decb9a83ca93a32595091b5c32d144675fb20
Favian <favouriteteo@gmail.com>
2023-04-09 17:24:53 +0800

    feat: add user auth with JWT

--- src/user/User.ts
+++ src/user/User.ts
@@ -1,14 +1,30 @@
-import { BaseEntity, Column, Entity, OneToMany, PrimaryColumn } from "typeorm";
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { instanceToPlain } from "class-transformer";
 
 import { Task } from "../task";
 
 @Entity()
 export class User extends BaseEntity {
-    @PrimaryColumn() id!: string;
+    @PrimaryGeneratedColumn("uuid")
+    readonly id!: string;
 
-    @Column() name!: string;
+    @Column()
+    readonly name!: string;
 
-    @Column() email!: string;
+    @Column()
+    readonly email!: string;
 
-    @OneToMany(() => Task, task => task.author) tasks!: Task[];
+    @Column()
+    readonly password!: string;
+
+    @OneToMany(() => Task, task => task.author)
+    readonly tasks!: Task[];
+
+    // TODO: ManyToMany => Boards
+    // @ManyToMany(() => Board, board.owner)
+    // readonly boards!: Board[];
 }
+
+export type UserLogin = Pick<User, "email" | "password">;
+export type UserRegister = Pick<User, "email" | "name" | "password">;
+export type UserDelete = Pick<User, "email" | "password">;
